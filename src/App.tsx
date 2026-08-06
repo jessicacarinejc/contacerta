54c6c52e908b80af9ed246febab92a3ae62e923a
jessicacarinejc <jessicacarine.jc@gmail.com>
2026-08-06 12:19:51 -0300

    feat: protect application routes with local authentication

--- src/App.tsx
+++ src/App.tsx
@@ -1,1 +1,44 @@
-import {HashRouter,Route,Routes} from 'react-router-dom';import {AppShell} from './components/layout/AppShell';import {AccountsPage} from './pages/AccountsPage';import {AssetsPage} from './pages/AssetsPage';import {BillsPage} from './pages/BillsPage';import {BudgetsPage} from './pages/BudgetsPage';import {CardsPage} from './pages/CardsPage';import {DashboardPage} from './pages/DashboardPage';import {DocumentsPage} from './pages/DocumentsPage';import {GoalsPage} from './pages/GoalsPage';import {IntelligencePage} from './pages/IntelligencePage';import {ReportsPage} from './pages/ReportsPage';import {SettingsPage} from './pages/SettingsPage';import {TransactionsPage} from './pages/TransactionsPage';export default function App(){return <HashRouter><AppShell><Routes><Route path="/" element={<DashboardPage/>}/><Route path="/movimentacoes" element={<TransactionsPage/>}/><Route path="/contas" element={<AccountsPage/>}/><Route path="/cartoes" element={<CardsPage/>}/><Route path="/contas-a-pagar" element={<BillsPage type="payable"/>}/><Route path="/contas-a-receber" element={<BillsPage type="receivable"/>}/><Route path="/documentos" element={<DocumentsPage/>}/><Route path="/orcamentos" element={<BudgetsPage/>}/><Route path="/metas" element={<GoalsPage/>}/><Route path="/patrimonio" element={<AssetsPage/>}/><Route path="/relatorios" element={<ReportsPage/>}/><Route path="/inteligencia" element={<IntelligencePage/>}/><Route path="/configuracoes" element={<SettingsPage/>}/><Route path="*" element={<DashboardPage/>}/></Routes></AppShell></HashRouter>}
+import { HashRouter, Route, Routes } from 'react-router-dom';
+import { AuthGate } from './components/auth/AuthGate';
+import { AppShell } from './components/layout/AppShell';
+import { ThemeController } from './components/ThemeController';
+import { AccountsPage } from './pages/AccountsPage';
+import { AssetsPage } from './pages/AssetsPage';
+import { BillsPage } from './pages/BillsPage';
+import { BudgetsPage } from './pages/BudgetsPage';
+import { CardsPage } from './pages/CardsPage';
+import { DashboardPage } from './pages/DashboardPage';
+import { DocumentsPage } from './pages/DocumentsPage';
+import { GoalsPage } from './pages/GoalsPage';
+import { IntelligencePage } from './pages/IntelligencePage';
+import { ReportsPage } from './pages/ReportsPage';
+import { SettingsPage } from './pages/SettingsPage';
+import { TransactionsPage } from './pages/TransactionsPage';
+
+export default function App() {
+  return (
+    <HashRouter>
+      <ThemeController />
+      <AuthGate>
+        <AppShell>
+          <Routes>
+            <Route path="/" element={<DashboardPage />} />
+            <Route path="/movimentacoes" element={<TransactionsPage />} />
+            <Route path="/contas" element={<AccountsPage />} />
+            <Route path="/cartoes" element={<CardsPage />} />
+            <Route path="/contas-a-pagar" element={<BillsPage type="payable" />} />
+            <Route path="/contas-a-receber" element={<BillsPage type="receivable" />} />
+            <Route path="/documentos" element={<DocumentsPage />} />
+            <Route path="/orcamentos" element={<BudgetsPage />} />
+            <Route path="/metas" element={<GoalsPage />} />
+            <Route path="/patrimonio" element={<AssetsPage />} />
+            <Route path="/relatorios" element={<ReportsPage />} />
+            <Route path="/inteligencia" element={<IntelligencePage />} />
+            <Route path="/configuracoes" element={<SettingsPage />} />
+            <Route path="*" element={<DashboardPage />} />
+          </Routes>
+        </AppShell>
+      </AuthGate>
+    </HashRouter>
+  );
+}
